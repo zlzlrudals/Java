cdc95a2282df10bf8832b21943d8734e7bf36fff
zlzlrudals <45863851+zlzlrudals@users.noreply.github.com>
2019-01-22 10:28:34 +0900

    Name change

---
+++ src/App12.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+
+public class App12 extends Applet {
+	public void paint(Graphics g) {
+		g.drawString("HELLO WORLD", 100, 50);
+	}
+
+}
--- src/Variable.java
+++
@@ -1,7 +0,0 @@
-
-public class Variable {
-	public static void main(String args[]) {
-		int i = 10;
-		System.out.println("i = "  +i);
-	}
-}
